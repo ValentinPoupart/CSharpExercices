1e64091838b8fc9338faaea8814de8861c7a0d8d
Poupart <v.poupart@riposte-consulting.fr>
2019-01-31 14:53:33 +0100

    partie permutation pas fonctionnelle

--- src/CS.Impl/04_Advanced/PermutationPrime.cs
+++ src/CS.Impl/04_Advanced/PermutationPrime.cs
@@ -1,26 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CS.Impl._04_Advanced
 {
     public class PermutationPrime
     {
+        private static IEnumerable<string> Permutate(string source)
+        {
+            if (source.Length == 1) return new List<string> { source };
+
+            var permutations = from c in source
+                               from p in Permutate(new String(source.Where(x => x != c).ToArray()))
+                               select c + p;
+
+            return permutations;
+        }
+
         public int[] GetPermutationPrimes(int upperBound)
         {
             try
             {
                 int[] result = new int[] { };
-                for (int i = 2; i != upperBound; i++)
+                for (int i = upperBound; i != 1; i--)
                 {
-                    var permutationsNumbers = PermutationsNumbers(i);
-                    foreach (int n in permutationsNumbers)
+                    var permutationsNumbers = Permutate(i.ToString());
+                    var testPrimeOnPermutation = true;
+                    foreach (string n in permutationsNumbers)
                     {
-                        IsPrime(n) ? continue : break;
+                        testPrimeOnPermutation = IsPrime(int.Parse(n)) && testPrimeOnPermutation && i.ToString().Length == n.Length; 
                     }
-                    if (IsPrime(i) && )
+                    if (testPrimeOnPermutation && IsPrime(i))
                     {
-                        result.Append(i);
-                        result.Concat(permutationsNumbers);
+                        if (!result.Contains(i))
+                            result = result.Append(i).ToArray();
+                        foreach (string n in permutationsNumbers)
+                        {
+                            var intParse = int.Parse(n);
+                            if (!result.Contains(intParse))
+                                result = result.Append(intParse).ToArray();
+                        }
                     }
                 }
                 return result;
@@ -31,27 +50,6 @@ namespace CS.Impl._04_Advanced
             }
         }
 
-        void permute(char* a, int l, int r)
-        {
-            int i;
-            if (l == r)
-                printf("%s\n", a);
-            else
-            {
-                for (i = l; i <= r; i++)
-                {
-                    swap((a + l), (a + i));
-                    permute(a, l + 1, r);
-                    swap((a + l), (a + i)); //backtrack 
-                }
-            }
-        }
-
-        private int[] PermutationNumber(int n)
-        {
-
-        }
-
         private bool IsPrime(int n)
         {
             return IsPrime(n, n / 2);
@@ -59,6 +57,10 @@ namespace CS.Impl._04_Advanced
 
         private bool IsPrime(int n, int current)
         {
+            if (current == 0)
+            {
+                return false;
+            }
             if (current == 1)
             {
                 return true;
--- src/CS.Impl/Program.cs
+++ src/CS.Impl/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CS.Impl
 {
@@ -6,7 +8,27 @@ namespace CS.Impl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int counter = 1;
+            for (var i = 1001; i != 1; i--)
+            {
+                Console.WriteLine($"{counter++}: ");
+                foreach (string str in Permutate(i.ToString()))
+                {
+                    Console.WriteLine($"{str}");
+                }
+            }
+            Console.WriteLine("end");
+        }
+
+        private static IEnumerable<string> Permutate(string source)
+        {
+            if (source.Length == 1) return new List<string> { source };
+
+            var permutations = from c in source
+                               from p in Permutate(new String(source.Where(x => x != c).ToArray()))
+                               select c + p;
+
+            return permutations;
         }
     }
 }
